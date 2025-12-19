3bb137f0f9bd105934a9493f3ec1c9b26f596f54
Sofie <sov.002@edu.zealand.dk>
2025-12-19 11:00:56 +0100

    loginpage

--- frontend/src/components/AdminHeader.vue
+++ frontend/src/components/AdminHeader.vue
@@ -1,11 +1,13 @@
 <template>
-  <header class="header-mobile">
-    <p>{{ $route.meta.Desc }}</p>
+  <header id="header-mobile" class="header-mobile">
+    <p >{{ $route.meta.Desc }}</p>
     <h1>{{ $route.meta.adminHeaderTitle }}</h1>
 
-    <nav class="header-desktop">
+    <nav class="nav-desktop">
+      <ul>
       <li>Dine lister</li>
       <li>Tilføj liste</li>
+      </ul>
 
     </nav>
   </header>
@@ -34,18 +36,47 @@ header {
 
   box-shadow: inset 8px -40px 15px rgba(0, 0, 0, 0.25);
 }
+.nav-desktop{
+  display: none;
+}
 .header-desktop{
   display: none;
 }
 
 @media(min-width: 700px){
 .header-mobile{
+  display:flex;
+border-radius: 0
+}
+.header-mobile p {
   display:none;
 }
-.header-desktop{
-  display: flex;
-  width: 100vw;
-  background: red;
+.header-mobile h1{
+  display:none;
+}
+.nav-desktop{
+  display:flex;
+  /* background:red; */
+  padding:16px;
+  list-style: none;
+
+}
+#header-mobile{
+  background: white;
+  box-shadow:none;
+height: 50px!important;
+display: flex;
+justify-content: center;
+color:green;
+;
+}
+header nav ul{
+  list-style: none;
+  display: flex; 
+  height: 100%;
+}
+header nav ul li{
+    padding: 16px;
 
 }
 }
--- frontend/src/views/SqlLogin.vue
+++ frontend/src/views/SqlLogin.vue
@@ -1,7 +1,10 @@
 <template>
   <main>
     <admin-header />
-    <v-sheet class="mx-auto" width="300">
+    <div class="content" display="flex" >
+<div class="form">
+  <h2 class="h2-login">Login</h2>
+<v-sheet  width="300" >
       <v-form @submit.prevent="login">
         <v-text-field v-model="Username" :rules="rules" label="Username"></v-text-field>
         <v-text-field
@@ -14,17 +17,33 @@
         <login-btn-form class="mt-2" color="#096123" type="submit" block></login-btn-form>
       </v-form>
     </v-sheet>
+</div>
+ 
+    <div class="add-mobile-green">
+    <img :src="backgroundImage" class="img"></img>
+    <h2 class="white">Download appen!</h2>
+    <v-btn>Download appen</v-btn>
+
+    </div>
+   
+
+    </div>
   </main>
+  <TheFooter/>
 </template>
 <script>
 import LoginBtnForm from "@/components/UI/LoginBtnForm.vue";
 import { login } from "@/api";
 import AdminHeader from "../components/AdminHeader.vue";
+import TheFooter from "../components/layout/TheFooter.vue";
+import backgroundImage from "@/assets/backgroundlogin.png";
+
 export default {
   name: "LoginForm",
   components: {
     LoginBtnForm,
     AdminHeader,
+    TheFooter
   },
 
   data() {
@@ -33,6 +52,7 @@ export default {
       UserPassword: "",
       rules: [(v) => !!v || "Field is required"],
       RegisterText: "Register",
+      backgroundImage
     };
   },
   methods: {
@@ -51,3 +71,122 @@ export default {
   },
 };
 </script>
+<style scoped>
+.form{
+  display:flex;
+  flex-direction: column;
+  /* justify-content: center;
+   */
+    
+}
+.content{
+/* background: red; */
+  display:flex;
+  flex-direction: column;
+  justify-content: center !important;
+  align-items:center;
+  width: 100vw;  
+  margin-top: ;
+}
+.add-mobile-green{
+  /* background-color:blanchedalmond; */
+  display:none;
+}
+header{
+  position: absolute;
+  height: 200px;
+z-index: 1;
+}
+main{
+  /* background-color:yellow; */
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  
+
+}
+
+.add-mobile{
+}
+@media (min-width: 700px) {
+  .form{
+  display:flex;
+  flex-direction: column!important;
+  justify-content: center!important;
+  width: 50vw;  
+height: 100%;
+    align-items:center!important;
+
+}
+main{
+  /* background-color:rgb(220, 220, 220); */
+  display: flex;
+  justify-content: center;
+  margin-top: 0px;
+  width: 100vw;
+
+}
+/* .add-mobile-green{
+  background-color:#096123;
+;
+  display:flex;
+width: 50vw;
+z-index: 1;
+justify-content: center !important;;
+align-items: center!important;
+flex-direction: column;
+height: 100%;
+} */
+ .add-mobile-green {
+  position: relative;
+  background-color: #096123;
+  width: 50vw;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden; /* vigtigt */
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  height: 100vh;
+}
+
+.add-mobile-green::before {
+  content: "";
+  position: absolute;
+  /* left: -120px;        trækker formen ind over venstre side */
+  top: 0;
+  width: 240px;
+  height: 100%;
+  background: #096123;
+  border-radius: 100% 0 0 100%;
+}
+.add-mobile-green > * {
+  position: relative;
+  z-index: 2;
+}
+
+
+.h2-login{
+ color: #096123;
+padding-top: 32px; 
+}
+.white{
+  color:white;
+  
+}
+.img{
+  z-index: 0;
+  position: absolute;
+  right:-10%;
+  width: 40%;
+  bottom:-10%;
+  width: 400px;
+}
+}
+</style>
